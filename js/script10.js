6fcf6dccd637faaccb79e1be3f99ffc79d9ff433
ruslan <ruslan_andreev@tut.by>
2021-06-10 18:31:28 +0300

    commit3

--- js/script10.js
+++ js/script10.js
@@ -2,8 +2,7 @@ let arrTask10 = [48,9,0,4,21,2,1,0,8,84,76,8,4,13,2],
     sumTask10 = 0,
     x = arrTask10.indexOf (0),
     y = arrTask10.lastIndexOf(0);
-console.log(x);
-console.log(y);
+
 if(x > 0 && y > 0){    
     for(i = x; i < y; i++){
         sumTask10 += arrTask10[i];
